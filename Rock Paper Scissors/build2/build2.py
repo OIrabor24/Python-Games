6742c15910afb8973ca070201123b26098bc6d7f
Osahon Irabor <osahonirabor@hotmail.com>
2022-08-10 15:03:17 -0700

    updated build2

--- Rock Paper Scissors/build2/build2.py
+++ Rock Paper Scissors/build2/build2.py
@@ -3,14 +3,14 @@ import random
 
 # Print multiline instruction
 # performstring concatenation of string
-print("Rules of the Game: \n"
-        +"Rock vs Paper -> Paper wins \n"
-        +   "Rock vs Scissors -> Rock wins \n"
-        +"Scissors vs Paper -> Scissors wins \n")
+print("Rules of the game: \n"
+        +"Rock vs. Paper -> Paper wins \n"
+        +"Rock vs. Scissors -> Rock wins \n"
+        +"Scissors vs. Paper -> Scissors wins \n")
 
 #while loop
 while True:
-    print("Enter your choice \n 1.Rock \n 2.Paper \n 3.Scissors \n")
+    print("Enter your choice: \n 1.Rock \n 2.Paper \n 3.Scissors \n")
 
 # take the input from user
     choice = int(input("Your turn: "))
@@ -19,8 +19,8 @@ while True:
 # if any one of the condition is true
 # then it return True value
 # looping until user enters invalid input
-    while choice > 3 or choice < 1:
-        choice = int(input("Enter a valid choice: "))
+    if choice < 1 or choice > 3:
+        choice = int(input("Enter a valid number: "))
 # initialize value of choice_name variable
 # corresponding to the choice value
     if choice == 1:
@@ -30,51 +30,54 @@ while True:
     else:
         choice_name = 'Scissors'
 
-    print("You selected, " + choice_name)
-    print("\nNow it's your opponents turn...")
 # print user choice
-    comp_choice = random.randint(1, 3)
+    print(f"You selected {choice_name}")
+    print("\nNow it's your opponents Turn...")
+
+
 # Computer chooses randomly any number
 # among 1 , 2 and 3. Using randint method
 # of random module
-    while comp_choice == choice:
-        comp_choice = random.randint(1, 3)
+    comp_choice = random.randint(1, 3)
 # looping until comp_choice value
 # is equal to the choice value
+    if comp_choice == choice:
+        comp_choice = random.randint(1,3)
+# initialize value of comp_choice_name
+# variable corresponding to the choice value
     if comp_choice == 1:
         comp_choice_name = 'Rock'
     elif comp_choice == 2:
         comp_choice_name = 'Paper'
     else:
         comp_choice_name = 'Scissors'
-# initialize value of comp_choice_name
-# variable corresponding to the choice value
-    print("Computer selects, " + comp_choice_name)
-    print(choice_name + " Vs. " + comp_choice_name)
+    
+    print(f"Computer selects {comp_choice_name}")
+    print(f"{choice_name} Vs. {comp_choice_name}") 
 
-    if ((choice == 1 and comp_choice == 2) or 
+# condition for winning
+    if ((choice == 1 and comp_choice == 2) or
     (choice == 2 and comp_choice == 1)):
-        print(" <==Paper Wins==> ", end="")
+        print(" <==Paper wins==> ", end="")
         result = 'Paper'
-    elif ((choice == 1 and comp_choice == 3) or 
+    elif ((choice == 1 and comp_choice == 3) or
     (choice == 3 and comp_choice == 1)):
-        print(" <==Rock Wins==> ", end="")
+        print(" <==Rock wins==> ", end="")
         result = 'Rock'
     else:
-        print(" <==Scissors Wins==> ", end="")
+        print(" <==Scissors wins==> ", end="")
         result = 'Scissors'
-# condition for winning
-    if result == choice_name:
-        print("You Win!")
-    else:
-        print("You Lose!")
 # Printing either user or computer wins
-    print("Do you want to play again?(Y/N): ")
-    answer = input() 
+    if result == choice_name:
+        print("\nYou win!")
+    else:
+        print("\nComputer wins!") 
+# if user input n or N then condition is True
+    print("Do you want to play again (Y/N)?")
 
+    answer = input()
     if answer == 'n' or answer == 'N':
         break 
-# if user input n or N then condition is True
 # after coming out of the while loop
 # we print thanks for playing
-print("\nThanks for playing Rock, Paper, Scissors!") 
+print("\nThanks for playing, Goodbye!") 
--- Rock Paper Scissors/practice/practice_4.py
+++ Rock Paper Scissors/practice/practice_4.py
@@ -1,20 +1,20 @@
 import random 
 
 def rock_paper_scissors():
-    player = input("Select 'r' for Rock, 'p' for Paper, or 's' for Scissors: ")
+    player = input("choose 'r' for Rock, 'p' for Paper, or 's' for Scissors: ")
     choices = ['r', 'p', 's']
-    opponent = random.choice(choices)
-
-    if player == opponent:
-        return print(f"It's a tie! You both selected {opponent}...")
+    opponent = random.choice(choices) 
 
     if winner(player, opponent):
-        return print(f"You win, {player} beats {opponent}!")
+        return print(f"You win! {player} beats {opponent}!")
+    
+    if player == opponent:
+        return print(f"It's a tie! you both selected {opponent}")
 
     if winner(player, opponent) != True:
-        return print(f"You lose! {opponent} beats {player}!")
+        return print(f"You lose, {opponent} beats {player}!") 
 
-def winner(user, cpu):
-    if (user == 'r' and cpu == 's') or (user == 'p' and cpu == 'r') or (user == 's' and cpu == 'p'):
+def winner(user, computer):
+    if (user == 'r' and computer == 's') or (user == 'p' and computer == 'r') or (user == 's' and computer == 'p'):
         return True 
 rock_paper_scissors() 
