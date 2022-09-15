0b5f7b86cfdae44683483abe1ee9f10ed4d8b169
Osahon Irabor <osahonirabor@hotmail.com>
2022-09-15 01:42:08 -0700

    made build11.py again

--- Rock Paper Scissors/build2/build11.py
+++ Rock Paper Scissors/build2/build11.py
@@ -12,13 +12,14 @@ while True:
     print("Choose: \n 1.Rock \n 2.Paper \n 3.Scissors \n")
 
 # take the input from user
-    choice = int(input("Enter your choice: "))
+    choice = int(input("Choose: "))
 # OR is the short-circuit operator 
 # if any one of the condition is true
 # then it return True value
 # looping until user enters invalid input
-    while choice > 3 or choice < 1:
-        choice = int(input("Please enter a valid number between 1-3: ")) 
+    if choice < 1 or choice > 3:
+        choice = int(input("Enter a number between 1-3")) 
+
 # initialize value of choice_name variable
 # corresponding to the choice value
     if choice == 1:
@@ -26,17 +27,15 @@ while True:
     elif choice == 2:
         choice_name = 'Paper'
     else:
-        choice_name = 'Scissors' 
-
-    print("You selected " + choice_name)
-    print("\nNow it's your opponents turn...") 
+        choice_name = 'Scissors'
    
     # print user choice
-
+    print("You picked " + choice_name)
+    print("Now it's your opponents turn...") 
 # Computer chooses randomly any number
 # among 1 , 2 and 3. Using randint method
 # of random module
-    comp_choice = random.randint(1, 3)
+    comp_choice = random.randint(1, 3) 
 # looping until comp_choice value
 # is equal to the choice value
     while comp_choice == choice:
@@ -49,32 +48,30 @@ while True:
         comp_choice_name = 'Paper'
     else:
         comp_choice_name = 'Scissors'
-    
-    print("You opponent selects "+ comp_choice_name)
-    print(choice_name + " Vs. " + comp_choice_name) 
+
+    print("The Computer picked " + comp_choice_name)
+    print(f"<=={choice_name}==> VS. <=={comp_choice_name}==>") 
 # condition for winning
-    if ((choice == 1 and comp_choice == 2) or
-    (choice == 2 and comp_choice == 1)):
-        print("Paper Wins\n", end= "")
+    if ((choice == 1 and comp_choice == 2) or (choice == 2 and comp_choice == 1)):
+        print("<==Paper wins==>\n")
         result = 'Paper'
-    elif ((choice == 1 and comp_choice == 3) or
-    (choice == 3 and comp_choice == 1)):
-        print("Rock Wins\n", end= "")
+    elif ((choice == 1 and comp_choice == 3) or (choice == 3 and comp_choice == 1)):
+        print("<==Rock Wins==>\n")
         result = 'Rock'
     else:
-        print("Scissors Wins\n", end= "")
-        result = 'Scissors'    
+        print("<==Scissors Wins==>\n")
+        result = 'Scissors' 
+
 # Printing either user or computer wins
     if result == choice_name:
-        print("You Win")
+        print("You Win!")
     else:
-        print("Computer Won")
-    
-    print("\nDo you want to play again? (Y/N)")
+        print("Computer Wins!")
+
+    answer = input("Do you want to play again? (Yes/No): ") 
 # if user input n or N then condition is True
-    answer = input()
-    if answer == 'n' or answer == 'N':
-        break
+    if answer == 'No' or answer == 'no':
+        break 
 # after coming out of the while loop
 # we print thanks for playing
-print("\nThanks for playing Rock, Paper, Scissors!")
+print("Thanks for playing!") 
